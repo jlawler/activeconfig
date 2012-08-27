becd6d04e772cf89b909cf7222a2b172c05253ef
Jeremy Lawler (tethys) <jeremylawler@gmail.com>
2012-08-26 21:19:07 -0500

    Fix rails env support for rails 3

--- lib/active_config/suffixes.rb
+++ lib/active_config/suffixes.rb
@@ -31,7 +31,15 @@ class ActiveConfig
        Socket.gethostname
       } 
       @symbols[:hostname_short]=proc {|sym_table| sym_table[:hostname].call(sym_table).sub(/\..*$/, '').freeze}
-      @symbols[:rails_env]=proc { |sym_table| (RAILS_ENV if defined?(RAILS_ENV))||ENV['RAILS_ENV']}
+      @symbols[:rails_env]=proc { |sym_table| 
+        if defined?(RAILS_ENV)
+          RAILS_ENV
+        elsif defined? Rails
+          Rails.env
+        else
+          ENV['RAILS_ENV']
+        end
+      }
       @symbols[:overlay]=proc { |sym_table| ENV['ACTIVE_CONFIG_OVERLAY']}
       @symbols.add_write_hook do
         ac_instance.flush_cache
