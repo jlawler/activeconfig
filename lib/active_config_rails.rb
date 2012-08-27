cf72e16db599b98710b76f6a05298d43af5ab95c
Jeremy Lawler (tethys) <jeremylawler@gmail.com>
2012-08-26 21:53:39 -0500

    Fixing generators and adding upgrade

--- lib/active_config_rails.rb
+++ lib/active_config_rails.rb
@@ -1,6 +1,30 @@
 
 module ActiveConfigRails
   module Generators
+    class ForceUpgradeGenerator < ::Rails::Generators::Base
+      namespace 'active_config:upgrade'
+      source_root File.expand_path('../active_config_rails/templates', __FILE__)
+
+      def generate_config
+        template 'active_config_initializer.rb', 'config/initializers/active_config.rb'
+      end
+
+      def insert_to_application
+        ln = "require File.expand_path('../initializers/active_config', __FILE__)\n"
+        append_to_file 'config/boot.rb', ln
+      end
+      def convert_database_yaml
+        copy_file 'database.yml', 'config/database.yml'
+        copy_file 'rails.yml', 'etc/rails.yml'
+
+
+        #comment = "\n  # Set the logging destination(s)\n  %s\n"
+        #insert_into_file 'config/environments/development.rb', comment % 'config.log_to = %w[stdout file]', :before => %r/^end\s*$/
+        #insert_into_file 'config/environments/production.rb', comment % 'config.log_to = %w[file]', :before => %r/^end\s*$/
+      end
+
+    end
+
     class InstallGenerator < ::Rails::Generators::Base
       namespace 'active_config:install'
       source_root File.expand_path('../active_config_rails/templates', __FILE__)
@@ -12,6 +36,7 @@ module ActiveConfigRails
       def insert_to_application
         ln = "require File.expand_path('../initializers/active_config', __FILE__)\n"
         prepend_to_file 'config/application.rb', ln
+        append_to_file 'config/boot.rb', ln
       end
       def convert_database_yaml
         empty_directory('etc')
--- lib/active_config_rails/templates/active_config_initializer.rb
+++ lib/active_config_rails/templates/active_config_initializer.rb
@@ -1,14 +1,29 @@
 require 'active_config'
+module ActiveConfigRails
+  def init_conf
+    if Object.const_defined? :CONF
+      Object.send(:remove_const, :CONF)
+    end
+  
+    etc_dir = File.dirname(File.expand_path(__FILE__)) + '/../../etc'
+    Object.const_set(:CONF,ActiveConfig.new(:path => etc_dir))
+  end
+  module_function :init_conf
+end
+ActiveConfigRails.init_conf
 
-if Object.const_defined? :CONF
-  Object.send(:remove_const, :CONF)
+if CONF.rails && CONF.rails.env and not ENV['ACTIVE_CONFIG_FORCE_RAILS_ENV']
+  ENV['ACTIVE_CONFIG_FORCE_RAILS_ENV']=CONF.rails.env
+  ENV['RAILS_ENV']||=CONF.rails.env
+  ActiveConfigRails.init_conf
 end
 
-etc_dir = File.dirname(File.expand_path(__FILE__)) + '/../../etc'
-Object.const_set(:CONF,ActiveConfig.new(:path => etc_dir))
-
-if CONF.rails.force_env and not ENV['ACTIVE_CONFIG_FORCE_RAILS_ENV']
+if CONF.rails && CONF.rails.force_env and not ENV['ACTIVE_CONFIG_FORCE_RAILS_ENV']
   ENV['ACTIVE_CONFIG_FORCE_RAILS_ENV']=CONF.rails.force_env
+  if ENV['RAILS_ENV']
+    STDERR.puts "WARNING:  ACTIVE CONFIG IS OVERRIDING YOUR ENVIRONMENT VARIABLE RAILS_ENV!\nPLEASE FIX YOUR rails.yml!"
+  end 
   ENV['RAILS_ENV']=CONF.rails.force_env
+  ActiveConfigRails.init_conf
 end
 
